238eafb6f47dae9fe5d18432d2f17105c262c448
mreck <mreck@users.noreply.github.com>
2020-12-01 14:31:11 +0100

    Create setup-basic.sh

---
+++ setup-basic.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget -O $HOME/.aliasrc   https://raw.githubusercontent.com/mreck/cfg/main/home/.aliasrc
+wget -O $HOME/.bashrc    https://raw.githubusercontent.com/mreck/cfg/main/home/.bashrc
+wget -O $HOME/.tmux.conf https://raw.githubusercontent.com/mreck/cfg/main/home/.tmux.conf
+wget -O $HOME/.vimrc     https://raw.githubusercontent.com/mreck/cfg/main/home/.vimrc
